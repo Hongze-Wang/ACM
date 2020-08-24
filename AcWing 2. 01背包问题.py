94c45f2088aa3ce0a2711eeeb2fb7ace2556bc13
Steve Wang <wanghongzecn@qq.com>
2020-08-24 18:36:19 +0800

    Update AcWing 2. 01背包问题.py

--- AcWing 2. 01背包问题.py
+++ AcWing 2. 01背包问题.py
@@ -9,16 +9,16 @@ goods = []
 for i in range(n):
     goods.append([int(i) for i in input().split()])
 
-dp = [[0]*(v+1) for _ in range(n)]
+dp = [[0]*(v+1) for _ in range(n+1)]
 
-for i in range(n):
+for i in range(1, n+1):
     for j in range(1, v+1):
-        if j < goods[i][0]:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-goods[i][0]] + goods[i][1])
+        dp[i][j] = dp[i-1][j] # if i < goods[i-1][0]
+        if j >= goods[i-1][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-goods[i-1][0]] + goods[i-1][1])
             
-print(dp[n-1][v])
+print(dp[-1][-1])
+
 # print(dp[-1][-1])
 
 # 单数组解法 是在二维数组解法基础上改进而来的 是编程节省空间的思考
@@ -34,8 +34,8 @@ for i in range(n):
 
 dp = [0] * (v+1)
 
-for i in range(n):
+for i in range(1, n+1):
     for j in range(v, -1, -1):
-        if j >= goods[i][0]:
-            dp[j] = max(dp[j], dp[j-goods[i][0]] + goods[i][1])
+        if j >= goods[i-1][0]:
+            dp[j] = max(dp[j], dp[j-goods[i-1][0]] + goods[i-1][1])
 print(dp[v])
