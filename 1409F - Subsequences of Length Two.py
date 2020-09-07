ddcc0c99f35f03f10aaf4a5e04678968c23da0ea
Steve Wang <wanghongzecn@qq.com>
2020-09-07 16:55:35 +0800

    Update 1409F - Subsequences of Length Two.py

--- 1409F - Subsequences of Length Two.py
+++ 1409F - Subsequences of Length Two.py
@@ -14,7 +14,7 @@ def solve(i, j, k):
         o = max(o, solve(i+1, j+1, k-c)) # (2) 使用t[0]修改之后最大子序列数目
     c = s[i] != t[1]     # 当前位置是否需要用t[1]修改
     if k >= c:           # 还有修改的机会
-        o = max(o, solve(i+1, j+(t[0] == t[1]), k-c) + j) # 使用t[1]修改之后最大子数列数目
+        o = max(o, solve(i+1, j+(t[0] == t[1]), k-c) + j) # (3) 使用t[1]修改之后最大子数列数目
     dp[i][j][k] = o      # 贪心策略 这里o已经是三种情况最大值
     return o
  
